20a167453fdcb21a7cfd294dbd6aff2b528c3845
karanwebdev <karanwebdev8@gmail.com>
2023-08-17 15:25:00 +0530

    .

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+echo "Hai Karan";
+
+
+#If Condition
+// if(condition){
+    //Type Your Code 
+// }elseif(Condition){
+    //Type Your Code
+// }else{
+// }
+
+#Logical Operators
+// && And
+// || Or
+// ! Not
+
+#
+?>

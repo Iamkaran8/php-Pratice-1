1c94b75019e3310cb73f2f7e752ca53f625e09f7
karanwebdev <karanwebdev8@gmail.com>
2023-08-18 15:09:36 +0530

    .

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-echo "Hai Karan";
+// echo "Hai Karan";
 
 
 #If Condition
@@ -15,5 +15,100 @@ echo "Hai Karan";
 // || Or
 // ! Not
 
-#
+#Switch Case
+// $grade = "A";
+
+// switch($grade){
+    
+//     case "A";
+//     echo "YOur Are Great";
+//     break;
+
+//     case "B";
+//     echo "YOur Are Good";
+//     break;
+
+//     case "C";
+//     echo "YOur Are OKey";
+//     break;
+//     default:
+//         echo "When Nothing Works";
+
+// }
+
+
+// $date = "Tuesday";
+// switch($date){
+
+//     case "Monday";
+//     echo "I Hate monnday";
+//     break;
+
+//     case "Tuesday";
+//     echo "I like monnday";
+//     break;
+
+//     case "Wednesday";
+//     echo "I love monnday";
+//     break;
+
+//     case "Thursday";
+//     echo "I will monnday";
+//     break;
+// }
+
+
+#For Loops
+// for($i =0; $i<5;$i++){
+//     echo "Hai Karan <h1>{$i}</h1> <br>";
+// }
+
+#While Loop
+//Condition When false It Program Will Stop
+// $num = 0;
+// while($num <10){
+//     echo "Hai THis is While Loop";
+//     break;
+// }
+
+#Array
+// $foods = Array("Apple","ORange","Banana","Coconet");
+
+// echo $foods[1];
+// foreach($foods as $a){
+//     echo $a;
+// }
+
+
+# Associative Array
+// $capitals = array(  "USA"=>"DC",
+//                     "Japan"=>"Kyoto",
+//                     "South Korea"=>"Seoul",
+//                     "India"=>"New De=elhi"
+// );
+
+// foreach($capitals as $cap => $key){
+//                         echo "<h1>{$cap}{$key} <br><br><h1>";
+//                     }
+
+
+#Isset() => Returns TRUE if a variable is declared and not null
+#Empty() => Returns TRUE if a variable is not declared, false null
+
+$name = "Karan";
+#Isset()
+// if(isset($name)){
+//     echo "Hai{$name}";
+// }else{
+//     echo "Name is Null";
+// }
+
+#empty()
+// if(empty($name)){
+//     echo "Name is Empty";
+// }else{
+//     echo "Name {$name}";
+// }
+
+
 ?>

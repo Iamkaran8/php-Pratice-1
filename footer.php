803855742fbae8922e2f9f7c9e28413449f50d46
karanwebdev <karanwebdev8@gmail.com>
2023-08-18 19:33:49 +0530

    .

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    <footer>
+        <h5>Author:Karan.S</h5>
+        <span>Copyright @2023</span>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 #Isset() => Returns TRUE if a variable is declared and not null
 #Empty() => Returns TRUE if a variable is not declared, false null
 
-$name = "Karan";
+// $name = "Karan";
 #Isset()
 // if(isset($name)){
 //     echo "Hai{$name}";
@@ -111,4 +111,138 @@ $name = "Karan";
 // }
 
 
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="Uname">Username:</label>
+        <input type="text" id="Uname" name="Uname">
+        <br><br>
+        <label for="Pass">Password:</label>
+        <input type="password" id="Pass" name="Pass">
+        <br><br>
+        <input type="submit" value="Submit"> -->
+    </form>
+
+    <?php
+
+    // $Uname = $_POST['Uname'];
+    // $Pass = $_POST['Pass'];
+    // echo "Username = {$Uname} <br>";
+    // echo "Password = {$Pass}";
+
+    #Isset(),Empty
+    // if(isset($Uname)){
+    //     echo "<br>This is Your User name{$Uname}";
+        
+    // }else{
+    //     echo "<br>There is no Username";
+    // }
+    ?>
+<!-- </body>
+</html> -->
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radio Buttons</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <input type="radio"   name="credit_card"  value="Visa"> Visa <br><br>
+    <input type="radio"   name="credit_card"  value="Mastercard"> Mastercard <br>
+    <input type="radio"   name="credit_card"  value="Rupay"> Rupay <br>
+    <input type="submit"  name="confirm"  value="Confirm">
+    </form>
+    <?php
+   
+    ?> // $Credit_card = $_POST["credit_card"];
+    // // echo $Credit_card;
+    // if(isset($Credit_card))
+</body>
+</html> -->
+<!-- 
+<form action="index.php" method="post">
+        <label for="Uname">Username:</label>
+        <input type="text" id="Uname" name="Uname">
+        <br><br>
+        <label for="Pass">Password:</label>
+        <input type="password" id="Pass" name="Pass">
+        <br><br>
+        <label for="Email">Email:</label>
+        <input type="text" id="Email" name="Email">
+        <br><br>
+        <input type="submit" value="Submit"> 
+    </form> -->
+
+    <?php
+
+    #Sanatize a User Input
+    // INPUT_POST,
+    // "Uname",
+    // FILTER_SANITIZE_SPECIAL_CHARS
+
+    // $Uname = $_POST['Uname'];
+    // $Pass = $_POST['Pass'];
+    // $Email = $_POST['Email'];
+    // $Email = filter_input(INPUT_POST,"Email",FILTER_SANITIZE_EMAIL);
+    // $Uname = filter_input(INPUT_POST,"Uname",FILTER_SANITIZE_SPECIAL_CHARS);
+    // echo "Username = {$Uname} <br>";
+    // echo "Password = {$Pass}";
+    // echo "Your Email Is {$Email}";
+
+    // #Isset(),Empty
+    // if(isset($Uname)){
+    //     echo "<br>This is Your User name{$Uname}";
+        
+    // }else{
+    //     echo "<br>There is no Username";
+    // }
+    // ?>
+
+    <!-- <form action="index.php" method="post">
+        <label >Age</label><input type="text" name ="Age" >
+        <br>
+        <input type="submit" value="submit">
+        <?php 
+            // $Age = $_POST["Age"];
+            // $Age = filter_input(INPUT_POST,"Age",FILTER_VALIDATE_INT);
+            // echo "This is YPur Age {$Age}";
+        ?>
+    </form> -->
+
+
+    <?php 
+    #Include()
+    // include("header.html");
+    ?>
+    <!-- <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home PAge</title>
+    </head>
+    <body>
+        
+    </body>
+    </html> -->
+
+    <?php 
+    // include("footer.php");
+    ?>
+
+
+<?php
+    #Cookie
+    setcookie("Name","Karan",time()+ (20),"/");
 ?>
